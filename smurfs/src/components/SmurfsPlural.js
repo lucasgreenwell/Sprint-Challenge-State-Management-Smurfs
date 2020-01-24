d8372a7d2e82d0ad8ae6e13d477562dc6ffac772
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-01-24 11:47:15 -0600

    i can delete a smurf

--- smurfs/src/components/SmurfsPlural.js
+++ smurfs/src/components/SmurfsPlural.js
@@ -1,10 +1,17 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import SmurfContext from '../contexts/contexts';
+import axios from 'axios';
 
 function SmurfsPlural(props) {
 
-
+    const [deleteThisSmurf, setDeleteThisSmurf] = useState({})
     const [smurfs, setSmurfs] = useContext(SmurfContext)
+
+    useEffect(()=> {
+        axios.delete(`http://localhost:3333/smurfs/${deleteThisSmurf.id}`)
+            .then(res => {console.log(res)})
+            .catch(err => {console.log(err)})
+    }, [deleteThisSmurf])
     return (
         <div>
             {smurfs.map(smurf => {
@@ -13,6 +20,9 @@ function SmurfsPlural(props) {
                  <div>name: {smurf.name}</div>
                  <div>height: {smurf.height}</div>
                  <div>age: {smurf.age}</div>
+                 <button onClick={() => {
+                     setDeleteThisSmurf({...smurf})
+                 }}>Delete Smurf</button>
              </div>
              )
             })}
